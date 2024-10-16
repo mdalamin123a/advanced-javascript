e8af5dd88492fa56333b05a16f769ed4257393d6
mdalamin123a <md.alamindev90@gmail.com>
2024-10-16 16:51:05 +0600

    null vs undefined cleared for interview

---
+++ 24-3-nullvsundefined.js
@@ -0,0 +1,29 @@
+let pakhi; // undefined
+
+// function add(num1, num2) { // return undefined
+//     console.log(num1 + num2);
+// }
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return; // return undefined
+// }
+
+// function add(num1, num2){ // return undefined
+//     console.log(num2);
+// }
+// const result = add(13);
+// console.log(result);
+
+// const premik = {name:"smart dude"};
+// console.log(premik.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+let exists = null;
+// console.log(exists);
+
+let ages = [2, 5, 9]; 
+console.log(ages[11]); // return undefined
+
